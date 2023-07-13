ebfeeed0ae6ec3c5bafd72854162d1eff51e8468
Saif Ali <89186408+SaifAliii@users.noreply.github.com>
2023-07-14 01:00:00 +0500

    completed challange 18 and learned about lodash

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -42,8 +43,12 @@ app.post("/", (req, res) => {
   res.render("home.ejs", { homeContent: homeStartingContent, posts: posts });
 });
 
-app.get("/posts/:param", (req, res) => {
-  console.log(req.params.param);
+app.get("/posts/:postTitle", (req, res) => {
+  posts.forEach(function (post) {
+    if (_.lowerCase(post.title) == _.lowerCase(req.params.postTitle)) {
+      console.log("Match Found");
+    }
+  });
 });
 
 app.listen(3000, function () {
